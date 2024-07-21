764ab54d7f700e92e84b7b563599c1e9fcd158e3
vinaybadola <vinaybadola.cs18@gmail.com>
2024-07-21 07:45:32 +0530

    code setup and create the view folder structure

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+    <h1>Hello</h1>
+    
+</div>
+
+</body>
+</html>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>
+    {{-- Stop trying to control. --}}
+
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+    <button wire:click="decrement">-</button>
+
+</div>
